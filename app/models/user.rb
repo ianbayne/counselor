0ed2995b5614ce65c4e8d55079e0605eaabc8c8c
Ian Bayne <ianbayne@gmail.com>
2017-10-24 19:01:55 +0900

    Create mailboxer index view

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :appointments
+
+  acts_as_messageable
+
+  def name
+    "User #{id}"
+  end
+
+  def mailboxer_email(object)
+    nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'users', controllers: { 
+  devise_for :users, path: 'users', controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
-  devise_for :counsellors, path: 'counsellors', controllers: { 
+  devise_for :counsellors, path: 'counsellors', controllers: {
     sessions: "counsellors/sessions",
     registrations: "counsellors/registrations"
   }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :conversations do
+    resources :messages
+  end
 end
