ee2e4e3a60f3b46715927b4116ddb53254594a5d
kazuya nakajima <uniss010@gmail.com>
2017-10-24 14:32:30 +0900

    create counsellor model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'users', controllers: { 
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+  devise_for :counsellors, path: 'counsellors', controllers: { 
+    sessions: "counsellors/sessions",
+    registrations: "counsellors/registrations"
+  }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
