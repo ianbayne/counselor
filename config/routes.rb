fce217406b063b889a073536a411a368dece226b
Enrique Naguiat <enzo_naguiat@yahoo.com>
2017-10-25 14:16:47 +0900

    added index action in questions controller, index route, and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'users', controllers: { 
+  devise_for :users, path: 'users', controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
-  devise_for :counsellors, path: 'counsellors', controllers: { 
+  devise_for :counsellors, path: 'counsellors', controllers: {
     sessions: "counsellors/sessions",
     registrations: "counsellors/registrations"
   }
   root to: 'pages#home'
   resources :users
   resources :counsellors
+  resources :questions, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
