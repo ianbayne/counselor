867be7af9356478366e61e3db36d502e3812bcd5
kazuya nakajima <uniss010@gmail.com>
2017-10-25 15:25:14 +0900

    user sign up with counsellor

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
   
   include Accessible
   
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+    # by matching, limit @counsellors to three.
+    @counsellors = Counsellor.all
+  end
 
   # POST /resource
   # def create
@@ -41,14 +43,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :counsellor_id])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :counsellor_id])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
