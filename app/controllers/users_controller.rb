78994d43ef89a58d9848952dbcefdd8446e9cc28
Ian Bayne <ianbayne@gmail.com>
2017-10-26 15:49:42 +0900

    Trying to get the user controller to play nice with chat function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,18 @@ class UsersController < ApplicationController
   end
 
   def show
-    # @user = current_user
-    @user = User.find(6)
-    # @counsellor = @user.counsellor
-    @counsellor = User.find(7)
-    @conversation = @user.mailbox.conversations.participant(@counsellor).last
+    # Test Code
+    # @user = User.find(6)
+    # @counsellor = User.find(7)
+    # @conversation = @user.mailbox.conversations.participant(@counsellor).last
+
+
+    @user = current_user
+    @counsellor = @user.counsellor
+    if @user.mailbox.conversations.participant(@counsellor).last
+      @conversation = @user.mailbox.conversations.participant(@counsellor).last
+    else
+
+    end
   end
 end
