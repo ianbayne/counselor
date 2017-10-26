68127bcb13cc1462dc5b2faa4f6e913ada6b1fc9
Ian Bayne <ianbayne@gmail.com>
2017-10-26 13:40:44 +0900

    further work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @counsellor = @user.counsellor
+    @conversation = current_user.mailbox.conversations.first
+    # .find(params[:conversation_id])
   end
 end
