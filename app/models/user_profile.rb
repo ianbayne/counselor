b5b934fdd0f199d4cacdde50d66c9dec639e357a
kazuya nakajima <uniss010@gmail.com>
2017-10-26 11:22:29 +0900

    add user to answer, association is off for user_profile

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,5 +1,5 @@
 class UserProfile < ApplicationRecord
-  belongs_to :user
-  has_many :answers
-  has_many :questions, through: :answers
+  # belongs_to :user
+  # has_many :answers
+  # has_many :questions, through: :answers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025113939) do
+ActiveRecord::Schema.define(version: 20171026021249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 20171025113939) do
     t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
     t.index ["user_profile_id"], name: "index_answers_on_user_profile_id"
   end
 
@@ -202,6 +204,7 @@ ActiveRecord::Schema.define(version: 20171025113939) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "user_profiles"
+  add_foreign_key "answers", "users"
   add_foreign_key "appointments", "counsellors"
   add_foreign_key "appointments", "users"
   add_foreign_key "goals", "counsellors"
