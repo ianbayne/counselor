b5dd1cac3ec3a54debeabbe3368587232d0d2e00
Ian Bayne <ianbayne@gmail.com>
2017-10-26 13:54:09 +0900

    Changes to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user
-    @counsellor = @user.counsellor
-    @conversation = current_user.mailbox.conversations.first
-    # .find(params[:conversation_id])
+    # @user = current_user
+    @user = User.find(6)
+    # @counsellor = @user.counsellor
+    @counsellor = User.find(7)
+    @conversation = @user.mailbox.conversations.participant(@counsellor)
   end
 end
