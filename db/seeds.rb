8136655b93a5c99503d4279dfd488522c0d2005b
kazuya nakajima <uniss010@gmail.com>
2017-11-01 13:06:21 +0900

    change seed for demo(6 people)

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,76 +8,242 @@
 
 p "start seeding."
 
+Appointment.destroy_all
+Mood.destroy_all
+Question.destroy_all
 User.destroy_all
 Counsellor.destroy_all
-Question.destroy_all
-Appointment.destroy_all
 
 i = 1
 p "Seed counsellors"
-p "first ten counsellors are male, last ten counsellors are female"
+# p "first ten counsellors are male, last ten counsellors are female"
 p "specialty_list for counsellor as below."
 p Counsellor::SPECIALTY
 
-20.times do
+p "counsellors for demo, 3 male and 3 female."
+p "Please choose Work, Relationship, Loss and No preference for gender"
+p "to have consistent mathing result."
 
-  specialty_list = Counsellor::SPECIALTY.shuffle
+counsellor = Counsellor.new(
+    first_name: "Kazuya",
+    last_name: "Nakajima",
+    age: 31,
+    gender: 0,
+    email: "counsellor_kazuya@gmail.com",
+    password: "123456",
+    photo: File.open(File.join(Rails.root, "app/assets/images/counsellor_m1.png"))
+    )
 
-  counsellor = Counsellor.new(
-    # first_name: "first_name#{i}",
-    # last_name: "last_name#{i}",
-    age: (30..60).to_a.sample,
-    gender: i <= 10 ? 0 : 1,
-    email: "counsellor#{i}@test.com",
-    password: "123456")
+counsellor.specialty_list.add([:Work, :Relationship, :Loss])
 
-  photo_num = i <= 10 ? i : i - 10
-  photo_gender = i <= 10 ? "m" : "f"
-  photo_path = "app/assets/images/counsellor_#{photo_gender}#{photo_num}.png"
-  p "photo_path is ", photo_path
-  photo = File.open(File.join(Rails.root, photo_path))
-  counsellor.photo = photo
+introduction = "Hi there.
+My name is Kazuya and I am a Mental Health and Addictions Counselor licensed in Japan with over 8 years of clinical experience.
+I hold a Masters of Arts in Counseling Psychology.
+I am also certified with the National Board of Certified Counselors.
+I have worked with clients with a wide range of concerns
+including depression, anxiety, grief, relationship issues, addiction problems, career challenges, and life transitions.
+I have also helped individuals with a history of trauma, including emotional and physical abuse."
 
-  # add 3 random speciality to counsellor from specilty list
-  3.times do
-    counsellor.specialty_list.add(specialty_list.pop)
-  end
+counsellor.introduction = introduction
+counsellor.save!
 
-  # random Japanese name to counsellor based on gender
-  gimei = counsellor.gender == 0 ? Gimei.male : Gimei.female
-  roman_name = gimei.romaji.split(" ")
-  first_name = roman_name[0]
-  last_name = roman_name[1]
-  counsellor.first_name = first_name
-  counsellor.last_name = last_name
+p "counsellor#{i} finished"
+i += 1
 
-  # url = ["https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/boy-512.png",
-  #       "https://cdn2.iconfinder.com/data/icons/person-gender-hairstyle-clothes-variations/48/Female-Side-comb-O-neck-512.png"]
-  # counsellor.remote_photo_url = url[counsellor.gender]
-  counsellor.save!
+gimei = Gimei.male
 
-  # introduction
-  introduction = "Hi there.
-  My name is #{counsellor.first_name} and I am a Mental Health and Addictions Counselor licensed in Japan with over 8 years of clinical experience.
-  I hold a Masters of Arts in Counseling Psychology.
-  I am also certified with the National Board of Certified Counselors.
-  I have worked with clients with a wide range of concerns
-  including depression, anxiety, grief, relationship issues, addiction problems, career challenges, and life transitions.
-  I have also helped individuals with a history of trauma, including emotional and physical abuse."
+counsellor = Counsellor.new(
+    first_name: gimei.romaji.split(" ")[0],
+    last_name: gimei.romaji.split(" ")[1],
+    age: 42,
+    gender: 0,
+    email: "counsellor_2@gmail.com",
+    password: "123456",
+    photo: File.open(File.join(Rails.root, "app/assets/images/counsellor_m2.png"))
+    )
 
-  counsellor.introduction = introduction
+counsellor.specialty_list.add([:Work, :Relationship, :Financial])
 
-  p "create counsellor\##{i}"
-  p "gender", counsellor.gender
-  p "first_name", counsellor.first_name
-  p "last_name", counsellor.last_name
-  p "introduction", counsellor.introduction
-  p "age", counsellor.age
-  p "photo_url", counsellor.photo
-  p "email:", counsellor.email
-  p "password:", counsellor.password
-  i += 1
-end
+introduction = "I am a Licensed Professional Counselor in the Tokyo, Japan. 
+I received a Master's Degree in Counseling Psychology at Avila University in Kansas City, 
+Missouri. I added a certification as a Personal Trainer through Smart Fitness because mental health, 
+diet and exercise go hand in hand. "
+
+counsellor.introduction = introduction
+counsellor.save!
+
+p "counsellor#{i} finished"
+i += 1
+
+gimei = Gimei.male
+
+counsellor = Counsellor.new(
+    first_name: gimei.romaji.split(" ")[0],
+    last_name: gimei.romaji.split(" ")[1],
+    age: 42,
+    gender: 0,
+    email: "counsellor_3@gmail.com",
+    password: "123456",
+    photo: File.open(File.join(Rails.root, "app/assets/images/counsellor_m3.png"))
+    )
+
+counsellor.specialty_list.add([:Work, :Relationship, :Financial])
+
+introduction = "I am a licensed mental health counselor in Tokyo with over 3 years work in community mental health 
+and 14 years experience in private practice, including 10 years as a certified clinical trainer for other counselors. 
+I work a great deal with adult survivors of childhood abuse, but also with those dealing with depression and anxiety."
+
+counsellor.introduction = introduction
+counsellor.save!
+
+p "counsellor#{i} finished"
+i += 1
+
+gimei = Gimei.female
+
+counsellor = Counsellor.new(
+    first_name: gimei.romaji.split(" ")[0],
+    last_name: gimei.romaji.split(" ")[1],
+    age: 42,
+    gender: 1,
+    email: "counsellor_4@gmail.com",
+    password: "123456",
+    photo: File.open(File.join(Rails.root, "app/assets/images/counsellor_f1.png"))
+    )
+
+counsellor.specialty_list.add([:Work, :Relationship, :Loss])
+
+introduction = "I have a warm and friendly style 
+and strongly believe in the importance of the therapeutic bond 
+and building a healthy and trusting relationships with all of my clients. 
+When working with me you can expect me to take a strong interest in your case 
+and concern for your well-being. I own a sucessful private practice,
+and am looking forward to bringing my services to many of you through BetterHelp's online counseling services. 
+Take care and I look forward to working with you!"
+
+counsellor.introduction = introduction
+counsellor.save!
+
+p "counsellor#{i} finished"
+i += 1
+
+gimei = Gimei.female
+
+counsellor = Counsellor.new(
+    first_name: gimei.romaji.split(" ")[0],
+    last_name: gimei.romaji.split(" ")[1],
+    age: 42,
+    gender: 1,
+    email: "counsellor_5@gmail.com",
+    password: "123456",
+    photo: File.open(File.join(Rails.root, "app/assets/images/counsellor_f2.png"))
+    )
+
+counsellor.specialty_list.add([:Work, :Relationship, :Loss])
+
+introduction = "I have a warm and friendly style 
+and strongly believe in the importance of the therapeutic bond 
+and building a healthy and trusting relationships with all of my clients. 
+When working with me you can expect me to take a strong interest in your case 
+and concern for your well-being. I own a sucessful private practice,
+and am looking forward to bringing my services to many of you through Counsellor.tokyo online counseling services. 
+Take care and I look forward to working with you!"
+
+counsellor.introduction = introduction
+counsellor.save!
+
+p "counsellor#{i} finished"
+i += 1
+
+gimei = Gimei.female
+
+counsellor = Counsellor.new(
+    first_name: gimei.romaji.split(" ")[0],
+    last_name: gimei.romaji.split(" ")[1],
+    age: 42,
+    gender: 1,
+    email: "counsellor_6@gmail.com",
+    password: "123456",
+    photo: File.open(File.join(Rails.root, "app/assets/images/counsellor_f3.png"))
+    )
+
+counsellor.specialty_list.add([:Work, :Relationship, :Financial])
+
+introduction = "I'm a Licensed Professional Counselor 
+and an Internationally Certified Alcohol and Drug Counselor. 
+I have worked in a variety of settings to include both mental health 
+and addiction treatment facilities. 
+I have over 15 years experience in the counseling field addressing depression, anxiety, anger, trauma/abuse and addiction. 
+I am also very comfortable providing crisis interventions to those 
+that may be experiencing immediate distress!"
+
+counsellor.introduction = introduction
+counsellor.save!
+
+p "counsellor#{i} finished"
+i += 1
+
+# disable normal counsellor seed
+# if false
+# 20.times do
+#   specialty_list = Counsellor::SPECIALTY.shuffle
+
+#   counsellor = Counsellor.new(
+#     # first_name: "first_name#{i}",
+#     # last_name: "last_name#{i}",
+#     age: (30..60).to_a.sample,
+#     gender: i <= 10 ? 0 : 1,
+#     email: "counsellor#{i}@test.com",
+#     password: "123456")
+
+#   photo_num = i <= 10 ? i : i - 10
+#   photo_gender = i <= 10 ? "m" : "f"
+#   photo_path = "app/assets/images/counsellor_#{photo_gender}#{photo_num}.png"
+#   p "photo_path is ", photo_path
+#   photo = File.open(File.join(Rails.root, photo_path))
+#   counsellor.photo = photo
+
+#   # add 3 random speciality to counsellor from specilty list
+#   3.times do
+#     counsellor.specialty_list.add(specialty_list.pop)
+#   end
+
+#   # random Japanese name to counsellor based on gender
+#   gimei = counsellor.gender == 0 ? Gimei.male : Gimei.female
+#   roman_name = gimei.romaji.split(" ")
+#   first_name = roman_name[0]
+#   last_name = roman_name[1]
+#   counsellor.first_name = first_name
+#   counsellor.last_name = last_name
+
+#   # url = ["https://cdn3.iconfinder.com/data/icons/rcons-user-action/32/boy-512.png",
+#   #       "https://cdn2.iconfinder.com/data/icons/person-gender-hairstyle-clothes-variations/48/Female-Side-comb-O-neck-512.png"]
+#   # counsellor.remote_photo_url = url[counsellor.gender]
+#   counsellor.save!
+
+#   # introduction
+#   introduction = "Hi there.
+#   My name is #{counsellor.first_name} and I am a Mental Health and Addictions Counselor licensed in Japan with over 8 years of clinical experience.
+#   I hold a Masters of Arts in Counseling Psychology.
+#   I am also certified with the National Board of Certified Counselors.
+#   I have worked with clients with a wide range of concerns
+#   including depression, anxiety, grief, relationship issues, addiction problems, career challenges, and life transitions.
+#   I have also helped individuals with a history of trauma, including emotional and physical abuse."
+
+#   counsellor.introduction = introduction
+
+#   p "create counsellor\##{i}"
+#   p "gender", counsellor.gender
+#   p "first_name", counsellor.first_name
+#   p "last_name", counsellor.last_name
+#   p "introduction", counsellor.introduction
+#   p "age", counsellor.age
+#   p "photo_url", counsellor.photo
+#   p "email:", counsellor.email
+#   p "password:", counsellor.password
+#   i += 1
+# end
+# end
 
 # QUESTIONS#
 p "Seed questions"
@@ -117,6 +283,9 @@ p "create question \##{question.id}"
 p "content:", question.content
 p "active: #{question.active}"
 
+p "no seed user for demo"
+
+if false
 p "Seed users"
 i = 1
 5.times do
@@ -141,10 +310,11 @@ i = 1
   p "counsellor:", user.counsellor.email
   i += 1
 end
+end
 
-
-p "Seed Appointments"
-
+p "no seed appointments as there are no users available"
+# p "Seed Appointments"
+if false
 Appointment.create(
   start_time: DateTime.new(2017,10,26,11),
   end_time: DateTime.new(2017,10,26,12),
@@ -170,5 +340,6 @@ Appointment.create(
   end_time: DateTime.new(2017,10,24,19),
   user_id: User.all.sample.id,
   counsellor_id: Counsellor.all.sample.id)
+end
 
 p "Finished seeding."
